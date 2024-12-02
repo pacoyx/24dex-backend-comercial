9a3864a6c66d3a674f9cff04b2d4aad955914799
carlos bazan <pacoyx@gmail.com>
2024-12-01 23:56:56 -0500

    cambio en el origins para aceptar todos forzando

--- Program.cs
+++ Program.cs
@@ -12,7 +12,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("Jwt"));
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
